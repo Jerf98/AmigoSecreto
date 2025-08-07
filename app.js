443d5640dd93abeed979d30eb347b647655d9d33
jerf983012@gmail.com <jerf983012@gmail.com>
2025-08-06 21:38:11 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let arrayAmigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Escribe un nombre válido");
+        return;
+    }
+
+    arrayAmigos.push(nombre);
+    input.value = "";
+
+    listaAmigo();
+}
+
+function listaAmigo() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    arrayAmigos.forEach(function(amigo) {
+        let listadoAmigo = document.createElement("li");
+        listadoAmigo.textContent = amigo;
+        lista.appendChild(listadoAmigo);
+    });
+}
+
+function sortearAmigo() {
+    const resultado = document.getElementById("resultado");
+    let i = Math.floor(Math.random() * arrayAmigos.length);
+    let amigoSecreto = arrayAmigos[i];
+
+    if (arrayAmigos.length === 0) {
+        alert("No hay amigos para sortear");
+        return;
+    }
+
+    resultado.textContent = `El amigo secreto es: ${amigoSecreto}`;
+}
